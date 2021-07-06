b8f9349121297fecb9a6757988333d8c12375106
amitiit11 <54931733+amitiit11@users.noreply.github.com>
2021-07-07 02:41:24 +0530

    Add files via upload

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Microsoft Surface Pro 7',
+    currency: 'Rs',
+    price: 72999,
+    image: 'images/01.jpg',
+  },
+  {
+    id: 2,
+    name: 'MacBook Pro 2021',
+    price: 179405,
+    currency: 'Rs',
+    image: 'images/02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Sony MDR',
+    price: 2999,
+    currency: 'Rs',
+    image: 'images/04.jpg',
+  },
+  {
+    id: 6,
+    name: 'Sony a7 III',
+    price: 179999,
+    currency: 'Rs',
+    image: 'images/06.jpg',
+  },
+  {
+    id: 5,
+    name: 'Boat Stone 1500',
+    price: 4599,
+    currency: 'Rs',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'OnePlus 9 Pro 5G ',
+    price: 59999,
+    currency: 'Rs',
+    image: 'images/03.jpg',
+  }
+];
+
+export default data;
